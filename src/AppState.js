a46702adaf7d1a39c89ca4bf3d577644136ceea2
bhuber2010 <bhuber@alumni.colostate.edu>
2016-08-18 23:44:31 -0700

    setup express server for api data consumption

--- src/AppState.js
+++ src/AppState.js
@@ -1,15 +1,20 @@
 import { observable, action } from 'mobx';
 
+const apiKey = '0f91ca9d-b649-483f-bfa4-af20bd40bc32'
+
 class AppState {
   @observable selectedTeam = null
-  @observable apidata = ["Game1", "Game2"]
+  @observable apidata = ['Game1', 'Game2']
 
   constructor() {
-
+    this.fetchData()
   }
 
-  @action('toggle timmer')
-  toggleTimer() {
+  @action('fetch api data')
+  fetchData() {
+    window.fetch('http://localhost:3333/games')
+      .then(games => console.log(games.body))
+      .catch(e => console.log(e))
 
   }
 
---
+++ srv/routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const unirest = require("unirest");
+const apiKey = '0f91ca9d-b649-483f-bfa4-af20bd40bc32';
+
+router.get('/games', function(req, res) {
+  const request = unirest("GET", "https://jsonodds.com/api/odds/nfl");
+  request.query({
+    "oddType": "Game",
+    "SiteID": "Bookmaker"
+  });
+  request.headers({
+    "cache-control": "no-cache",
+    "jsonodds-api-key": apiKey
+  });
+
+  request.end(response => {
+    if (response.error) throw new Error(response.error);
+    console.log(response);
+    res.send(response.body)
+  });
+
+});
+
+module.exports = router;
